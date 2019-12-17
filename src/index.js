aa0ad619c7f81a1d4f006225835640a59917952a
Johnathan Brunelle <jbrunel2@uwo.ca>
2019-12-17 01:41:30 -0500

    Webpack template

---
+++ src/index.js
@@ -0,0 +1,15 @@
+console.log('Hello Webpack Project.');
+
+class App {
+    constructor(){
+
+    }
+
+    buttonTest() {
+        console.log("Hello World!");
+
+    }
+}
+
+// Create a new app globally
+window.app = new App();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'Hello Webpack bundled JavaScript Project',
+        template: './src/index.html'
+      })
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
